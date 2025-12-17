46f4d8753ece3acafa8dc0aa2b9885aa3aba66fa
kkobrakai941 <Amir2016kzn@gmail.com>
2025-12-17 10:23:07 -0800

    Добавлена перезагрузка модели в DAG

--- airflow/dags/model_retrain_dag.py
+++ airflow/dags/model_retrain_dag.py
@@ -7,14 +7,21 @@ from sklearn.datasets import load_iris
 import pickle
 
 
+import requests
 def train_model():
     data = load_iris()
     X = data.data
     y = data.target
     model = LogisticRegression(max_iter=200)
+ 
     model.fit(X, y)
     with open('/models/model.pkl', 'wb') as f:
         pickle.dump(model, f)
+
+
+def reload_model_task():
+    response = requests.post('http://ml_service:8000/reload_model')
+    response.raise_for_status()
 
 
 with DAG(
@@ -27,3 +34,8 @@ with DAG(
         task_id='train_model',
         python_callable=train_model
     )
+    reload_model = PythonOperator(
+        task_id='reload_model',
+        python_callable=reload_model_task
+    )
+    train >> reload_model
